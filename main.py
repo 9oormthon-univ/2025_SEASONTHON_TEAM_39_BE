a218d9e7f7ac276fd7a59cea51b6b49bb5daffd5
Yeone-in <ye1andonly@gmail.com>
2025-09-07 03:47:00 +0900

    [FEATURE] 여러 곡 검색 기능 추가

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-#/dev/spotify/recom.py
+'''최종
+C:/dev/spotify
+python -m uvicorn recom:app --reload
+'''
+
 from fastapi import FastAPI
 import os
 import httpx
@@ -16,8 +20,8 @@ SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 
 sp = Spotify(auth_manager=SpotifyClientCredentials(
-    client_id=SPOTIFY_CLIENT_ID,
-    client_secret=SPOTIFY_CLIENT_SECRET
+    client_id=os.getenv("SPOTIFY_CLIENT_ID"),
+    client_secret=os.getenv("SPOTIFY_CLIENT_SECRET")
 ))
 
 # 입력 JSON 모델 정의
@@ -58,32 +62,42 @@ def healthcheck():
 @app.post("/recommend")
 def recommend_song(data: EmotionInput, limit: int = 10):
     """
-    main_emotion으로 seed track을 검색한 뒤,
-    energy_level, valence 기준으로 추천곡 반환!
+    main_emotion으로 seed track을 검색
     """
     try:
         # 1. main_emotion으로 트랙 검색
         search_result = sp.search(q=data.main_emotion, type="track", limit=limit)
-        if not search_result["tracks"]["items"]:
+        items = search_result["tracks"]["items"]
+        if not items:
             return {"error": f"No tracks found for emotion: {data.main_emotion}"}
 
-        seed_track_id = search_result["tracks"]["items"][0]["id"]
-        
-        
+        results = []
+        for track in items:
+             results.append({
+                "name": track["name"],
+                "artist": ", ".join([a["name"] for a in track["artists"]]),
+                "url": track["external_urls"]["spotify"]
+            })
+
+        return {
+            "tracks": results}
+
+        '''
         # 2. 추천곡 검색 
         recs = sp.track(
             track_id=seed_track_id,
             #limit=limit,
             #target_energy=data.energy_level,
             #target_valence=data.valence
-        )
+        )'''
 
         name = recs["name"]
         artist = recs["artists"][0]["name"]
         url = recs["external_urls"]["spotify"]
 
-        return name, artist, url
-    
+        #return name, artist, url
+        return recs
+
         '''    results = []{
                 "name": recs["name"],
                 #search_result["tracks"]["items"][0]["name"],
